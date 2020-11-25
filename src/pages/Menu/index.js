0f07d5adde55e3136ecaa2891c9634fe1d0ebcb1
Fryzenn <fryzenn@gmail.com>
2020-11-25 16:58:48 +0000

    Updates

--- src/pages/Menu/index.js
+++ src/pages/Menu/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import { View, Image, Text, TextInput, TouchableOpacity } from 'react-native';
+import { ImageBackground, View, Image, Text, TextInput, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import logoImg from '../../assets/logo.png';
-import logoIpb from '../../assets/ipb.png';
+import bgImg from '../../assets/bg.png';
+import back from '../../assets/back.png';
+import rectangle from '../../assets/Rectangle.png';
+import seta from '../../assets/seta.png';
+import settings from '../../assets/settings.png';
+import mensagem from '../../assets/mensagem.png';
 
 import styles from './styles';
 
 export default function Menu(){
     const navigation = useNavigation();
+
+    function navigateInicio(){
+        navigation.navigate('Inicio');
+    }
+
+    function navigateLogin(){
+        navigation.navigate('Login');
+    }
 
     function navigateReparacao(){
         navigation.navigate('Reparacao');
@@ -22,37 +35,76 @@ export default function Menu(){
         navigation.navigate('Admin');
     }
 
-    function navigateLogin(){
-        navigation.navigate('Login');
-    }
-
     return (
-        <View style={styles.container}>  
 
-            <Text style={{ color: "#fff", fontWeight: "bold", fontSize: 13, marginBottom: 50 }}>Centro de Reparação de Eletrodomésticos</Text>
+        <ImageBackground source={bgImg} style={styles.backgroundImage}>
+        
+        <View style={styles.back}>             
 
-            <Image source={logoImg} style={{ width: 250, height: 250 }}/>
-
-            <Text style={{ color: "#fff", fontWeight: "bold", fontSize: 20}}>MENU</Text>
-
-            <TouchableOpacity style={styles.button} onPress={navigateReparacao}>
-                <Text style={{ fontSize: 18, color: "#fff" }}>Apresentar Reparação</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.button} onPress={navigateListar}>
-                <Text style={{ fontSize: 18, color: "#fff" }}>Listar Reparações</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.button} onPress={navigateAdmin}>
-                <Text style={{ fontSize: 18, color: "#fff" }}>Admin</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.button} onPress={navigateLogin}>
-                <Text style={{ fontSize: 18, color: "#fff" }}>Sair</Text>
-            </TouchableOpacity>
-
-            <Image source={logoIpb} style={{width: 300, height: 50, marginTop: 60}}/>
+                <TouchableOpacity onPress={navigateInicio}>
+                    <Image source={back} style={{ width: 35, height: 30 }}/>
+                </TouchableOpacity>
 
         </View>
+
+        <View style={styles.container}>  
+
+            <Image source={logoImg} style={{ width: 180, height: 140}}/>
+
+            <ImageBackground source={rectangle} style={{width: 370, height: 640}}>
+                
+                <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginTop: 15}}>
+                    <Text style={{ fontSize: 18, color: "#D63578", fontWeight: "bold", textAlign: 'center'}}>Bem-Vindo, </Text>
+                    <Text style={{ fontSize: 18, color: "#000", fontWeight: "bold", textAlign: 'center'}}>Nuno Gomes</Text>
+                </View>
+
+                <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+                    <Text style={{ fontSize: 18, color: "#D63578", fontWeight: "bold", textAlign: 'center'}}>Numero: </Text>
+                    <Text style={{ fontSize: 18, color: "#000", fontWeight: "bold", textAlign: 'center'}}>a38368</Text>
+                </View>
+
+                <View style={{flexDirection: 'row', alignItems: 'center', marginTop: 30}}>                    
+                    <Text style={{ fontSize: 18, fontWeight: "bold", color: "#000", marginLeft: 40,}}>Requisitar Equipamento</Text>
+                    <TouchableOpacity style={styles.button} onPress={navigateReparacao}>
+                        <Image source={seta} style={{ width: 40, height: 45, marginTop: -5, marginLeft: 20}}/>
+                    </TouchableOpacity>
+                </View>
+            
+                <View style={{flexDirection: 'row', alignItems: 'center', marginTop: 10}}>                    
+                    <Text style={{ fontSize: 18, fontWeight: "bold", color: "#000", marginLeft: 40,}}>Devolver Equipamento</Text>
+                    <TouchableOpacity style={styles.button} onPress={navigateReparacao}>
+                        <Image source={seta} style={{ width: 40, height: 45, marginTop: -5, marginLeft: 30}}/>
+                    </TouchableOpacity>
+                </View>
+                
+                <View style={{flexDirection: 'row', alignItems: 'center', marginTop: 10}}>                    
+                    <Text style={{ fontSize: 18, fontWeight: "bold", color: "#000", marginLeft: 40,}}>Login como Administrador</Text>
+                    <TouchableOpacity style={styles.button} onPress={navigateReparacao}>
+                        <Image source={seta} style={{ width: 40, height: 45, marginTop: -5, marginLeft: -5}}/>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginTop: 200, marginLeft: -35}}>                    
+                    
+                    <TouchableOpacity style={styles.butoes} onPress={navigateReparacao}>
+                        <Image source={settings} style={{width: 30, height: 30, marginRight: 15}}/>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.button1} onPress={navigateLogin}>
+                        <Text style={{fontSize: 13, color: "#fff", textAlign: 'center', marginTop: 12}}>Terminar Sessão</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.butoes} onPress={navigateReparacao}>
+                        <Image source={mensagem} style={{width: 40, height: 35, marginLeft: -25}}/>
+                    </TouchableOpacity>
+
+                </View>
+
+            </ImageBackground>
+
+        </View>
+
+        </ImageBackground>
+
     );
 }
--- src/pages/Menu/styles.js
+++ src/pages/Menu/styles.js
@@ -4,21 +4,41 @@ import Constants from 'expo-constants';
 export default StyleSheet.create({
     container: {
         flex: 1,
-        paddingHorizontal: 24,
-        paddingTop: Constants.statusBarHeight + 20,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#2c3e50"
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    back: {
+        marginTop: 40,
+        marginLeft: 20
+    },
+
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover',
+        justifyContent: 'center',
     },
 
     button: {
-        backgroundColor: "#66194d",
-        width: 250, 
-        height: 40,
-        marginTop: 15,        
-        borderRadius: 3,
-        justifyContent: "center",
-        alignItems: "center"
+        width: 370, 
+        height: 40, 
+        marginTop: 10,  
+        marginLeft: 40,   
+        borderRadius: 3
+    }, 
+
+    button1: {
+        backgroundColor: "#332003",
+        width: 200, 
+        height: 45, 
+        marginTop: 10,  
+        borderRadius: 3
+    },
+
+    butoes: {
+        marginLeft: 40,
+        marginTop: 8
     }
     
 });
